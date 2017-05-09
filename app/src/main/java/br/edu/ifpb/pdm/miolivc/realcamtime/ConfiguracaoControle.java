9b879d670d3cd536e7e5a2bf32aba65929c74179
Aluno <aluno@Elder-MacBook-Air.local>
2017-05-09 14:22:52 -0300

    Adicionando activities

---
+++ app/src/main/java/br/edu/ifpb/pdm/miolivc/realcamtime/ConfiguracaoControle.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pdm.miolivc.realcamtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfiguracaoControle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracao_controle);
+    }
+}
