4dcf5a31e9ab90302c91236f5a23197ce8c49a07
Ari Garcia <aristofanio@hotmail.com>
2017-05-11 21:44:44 -0300

    cam in background

--- app/src/main/java/br/edu/ifpb/pdm/miolivc/realcamtime/WebCam.java
+++ app/src/main/java/br/edu/ifpb/pdm/miolivc/realcamtime/WebCam.java
@@ -1,5 +1,6 @@
 package br.edu.ifpb.pdm.miolivc.realcamtime;
 
+import android.content.Intent;
 import android.hardware.Camera;
 import android.media.MediaRecorder;
 import android.os.AsyncTask;
@@ -7,33 +8,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Surface;
+import android.webkit.WebView;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 import br.edu.ifpb.pdm.miolivc.realcamtime.controlller.CameraPreview;
 
 public class WebCam extends AppCompatActivity {
 
     private Camera camera;
-    private MediaRecorder mediaRecorder;
     private CameraPreview cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_cam);
-
-        camera = Camera.open();
+        //
+        camera = Camera.open();//
+        //
         camera.setDisplayOrientation(90);
-        this.cameraPreview = new CameraPreview(getApplicationContext(), camera);
+        cameraPreview = new CameraPreview(getApplicationContext(), camera, new CameraPreview.CameraPreviewListener() {
+            @Override
+            public void exec() {
+                Bundle bundle = getIntent().getExtras();
+                if (bundle != null && bundle.containsKey("gravar") &&  bundle.getBoolean("gravar")) {
+                    cameraPreview.gravarVideo(new CameraPreview.CameraPreviewListener(){
+                        @Override
+                        public void exec() {
+                            WebCam.this.startActivity(new Intent(WebCam.this, MainActivity.class));
+                        }
+                    });
+                    //
 
+                }
+            }
+        });
+        //
         FrameLayout frameLayout = (FrameLayout) findViewById(R.id.webCamSurface);
         frameLayout.addView(cameraPreview);
-
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null && bundle.containsKey("gravar") &&  bundle.getBoolean("gravar")) {
-            cameraPreview.gravarVideo();
-        }
     }
 
 
--- app/src/main/java/br/edu/ifpb/pdm/miolivc/realcamtime/controlller/CameraPreview.java
+++ app/src/main/java/br/edu/ifpb/pdm/miolivc/realcamtime/controlller/CameraPreview.java
@@ -19,19 +19,26 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     private SurfaceHolder holder;
     private Camera camera;
+    private MediaRecorder mediaRecorder;
+    private CameraPreviewListener listener;
 
-    public CameraPreview(Context context, Camera camera) {
+    public CameraPreview(Context context, Camera camera, CameraPreviewListener listener) {
         super(context);
         this.camera = camera;
         this.camera.setDisplayOrientation(90);
-        holder = getHolder();
-        holder.addCallback(this);
+        this.holder = getHolder();
+        this.holder.addCallback(this);
+        this.mediaRecorder = new MediaRecorder();
+        this.listener = listener;
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
         try {
             camera.setPreviewDisplay(holder);
             camera.startPreview();
+            //
+            Log.d("AGDebug", "Surface created");
+            //
         } catch (IOException e) {
             Log.d("miolivc", "Error setting camera preview: " + e.getMessage());
         }
@@ -44,54 +51,56 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        //
         if (holder.getSurface() == null){
-            // preview surface does not exist
             return;
         }
+        //
         try {
             camera.stopPreview();
         } catch (Exception e){
+            Log.e("AGDebug", "Erro ao alterar o surface.", e);
         }
 
         try {
             camera.setPreviewDisplay(holder);
             camera.startPreview();
-
-        } catch (Exception e){
-            Log.d("miolivc", "Error starting camera preview: " + e.getMessage());
+            //
+            listener.exec();
+        }
+        catch (Exception e){
+            Log.e("AGDebug", "Error starting camera preview. ", e);
         }
     }
 
 
-    public void gravarVideo() {
-
-        MediaRecorder mediaRecorder = new MediaRecorder();
-
+    public void gravarVideo(final CameraPreviewListener cameraPreviewListener) {
+        //
+        if (mediaRecorder != null) {
+            mediaRecorder.release();
+        }
+        mediaRecorder = new MediaRecorder();
+        //
         camera.unlock();
         //
         mediaRecorder.setCamera(camera);
-
-        Log.e("asasas", "asssou pelac criacao do media");
-
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
         mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
-        //mediaRecorder.setMaxDuration(10000);
         //
-//        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "video.mp4");
-//        try {
-//            file.createNewFile();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        mediaRecorder.setOutputFile(file.getAbsolutePath());
-        mediaRecorder.setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO).toString());
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "video.mp4");
+        mediaRecorder.setOutputFile(file.getAbsolutePath());
         //
-        Log.e("iosd", "setou properties");
-
-        //mediaRecorder.setPreviewDisplay(CameraPreview.this.getHolder().getSurface());
-
-        Log.e("miolivc", "recuperou surface");
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+        //
+        mediaRecorder.setPreviewDisplay(getHolder().getSurface());
+        //
+        Log.e("AGDebug", "Configurou a camera.");
+        //
+        mediaRecorder.setMaxDuration(10000);
+        //
+        Log.e("AGDebug", "Definiu o surface.");
         //
         try {
             mediaRecorder.prepare();
@@ -99,7 +108,27 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
            Log.e("AGDebug", "Erro na camera", e);
         }
         //
+        Log.e("AGDebug", "Iniciando a gravação");
+        //
+        mediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener(){
+            @Override
+            public void onInfo(MediaRecorder mr, int what, int extra) {
+                if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED){
+                    mediaRecorder.stop();
+                    mediaRecorder.release();
+                    mediaRecorder = null;
+                    Log.e("AGDebug", "Finalizado a gravação");
+                    cameraPreviewListener.exec();
+                }
+            }
+        });
+        //
         mediaRecorder.start();
-
     }
+
+
+    public interface CameraPreviewListener {
+        void exec();
+    }
+
 }
